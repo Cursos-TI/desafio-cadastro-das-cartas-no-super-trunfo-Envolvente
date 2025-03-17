e81a95c296e0049f3dc49a38474a862bcef38edc
Envolvente <91615542fiel@gmail.com>
2025-03-17 22:17:13 +0000

    printf 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,13 +7,20 @@ int main() {
    char codigo, nome[20];
 
      
-  printf ("Bem vindo ao cadastro de cartas! \n");
-     
 
-  printf ("Informe seu estado:");
+  printf ("Bem vindo ao cadastro de cartas! \n");
+
+  printf ("Informe seu estado: \n ");
   scanf  ("&s" , &estado);
 
+  printf ("Informe o codigo da carta: \n");
+  scanf ("&s" , &codigo);
 
+  printf ("Informe o nome da cidade: \n");
+  scanf ("&s" , &nome);
+
+  printf ("Informe o numero de habitantes: \n");
+  scanf ("&d" , &população);
 
 
 
